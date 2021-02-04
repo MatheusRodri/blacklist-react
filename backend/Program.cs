3b9b0efee38e140fab900d9b99247759d903c669
Matheus Rodrigues De Jesus <mathclash25@gmail.com>
2021-02-04 20:12:17 -0300

    Update Program.cs

--- backend/Program.cs
+++ backend/Program.cs
@@ -16,11 +16,11 @@ namespace backend
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
+         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                });  
     }
 }
